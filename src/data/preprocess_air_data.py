de2f0a72e626a264910930c6d7f9cc5f36bc0540
Lara Rantuša <lara.rantusa@student.um.si>
2023-04-06 18:01:35 +0200

    format

--- src/data/preprocess_air_data.py
+++ src/data/preprocess_air_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 def preprocess_air_data(df):
     
     df_filtered = df.loc[df['merilno_mesto'] == 'MB Vrbanski'].copy()
-    df_filtered['datum_od'] = pd.to_datetime(df_filtered['datum_od'], unit="s")
+    df_filtered['datum_od'] = pd.to_datetime(df_filtered['datum_od'])
     df_air = df_filtered[['datum_od', 'pm10']].copy()
 
 
