7ff05c84a357f875dfbe638033ba31f97c45b8cc
Lara Rantuša <lara.rantusa@student.um.si>
2023-04-12 23:32:44 +0200

    Update yaml

--- src/data/fetch_weather_data.py
+++ src/data/fetch_weather_data.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from data.preprocess_weather_data import preprocess_weather_data
+from data.preprocess_air_data import preprocess_air_data
 
 
 def refactor_values(data):
@@ -24,15 +24,32 @@ def process_data(src, dist):
     raw = json.load(f)
     f.close()
 
+    df = pd.DataFrame()
 
-    df_weather = pd.DataFrame.from_dict(json.loads(json.dumps(raw["hourly"])))
+    print('Transforming json to pandas dataframe...')
+    # prilagodimo json dataframe-u
+    #for i in range(len(raw)):
+    #    jdata = json.loads(raw[i]['json'])
+    #    station = jdata['arsopodatki']['postaja']
+    #    for i in range(len(station)):
+    #        data = station[i]
+    #        data = refactor_values(data)
+    #        df = pd.concat([df, pd.json_normalize(data)])
+    for i in range(len(raw)):
+        data = raw[i]
+        dictData = json.loads(data['json'])
+    
+    df1 = pd.json_normalize(dictData['arsopodatki']['postaja'])
+    df = pd.concat([df, df1])#, axis=1, join="inner")
 
-    df_weather = preprocess_weather_data(df_weather)   
+    df = df.reset_index(drop=True)    
 
-    print('Saving processed weather data...')
-    df_weather.to_csv(dist, index=False)
+    df_air = preprocess_air_data(df)
 
-    print('Finished weather!')
+    print('Saving processed air data...')
+    df_air.to_csv(dist, index=False)
+
+    print('Finished air!')
 
 
 
@@ -40,13 +57,17 @@ if __name__ == '__main__':
 
     root_dir = os.path.abspath(os.path.join(
         os.path.dirname(__file__), '../..'))
+    
 
     src = os.path.join(root_dir, 'data', 'raw', 'air')
-    dist = os.path.join(root_dir, 'data', 'processed', 'processed_weather')
+    dist = os.path.join(root_dir, 'data', 'processed', 'processed_air')
 
-    if not os.path.exists(src):
-        os.makedirs(src)
+    if not os.path.exists(dist):
+        os.makedirs(dist)
 
+    # print(root_dir)
+    # print(src)
+    # print(dist)
 
     #src = os.path.join(root_dir, 'data', 'raw', 'data.json')
     #dist = os.path.join(root_dir, 'data', 'processed', 'data.csv')
