b405c9fea5ed63a7006fb297c88ec0e320a1bfe1
Lara Rantuša <lara.rantusa@student.um.si>
2023-04-12 23:22:57 +0200

    weatherCopy.json

--- src/data/fetch_weather_data.py
+++ src/data/fetch_weather_data.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     root_dir = os.path.abspath(os.path.join(
         os.path.dirname(__file__), '../..'))
 
-    src = os.path.join(root_dir, 'data', 'raw', 'weather')
+    src = os.path.join(root_dir, 'data', 'raw', 'weather_copy.json')
     dist = os.path.join(root_dir, 'data', 'processed', 'processed_weather')
 
     if not os.path.exists(src):
